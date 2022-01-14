0adacd5dd1479b2465d24e234553443afba3ff1e
Patrick Fernandes Peçanha <patrickFP93@gmail.com>
2022-01-14 03:46:12 -0300

    chage for desing from  bulma.css and make 'index' page.

--- cool-load/src/App.js
+++ cool-load/src/App.js
@@ -1,8 +1,63 @@
-import './App.css';
+import { Block, Button, Columns, Container, Heading, Image, Navbar, Notification } from "react-bulma-components";
 
 function App() {
   return (
-    <h1> Seja bem vindo ao app de gerenciamento de carga.</h1>
+    <>
+      <Container style={{ marginTop: "50px" }}>
+        <Columns>
+          <Columns.Column size={2} offset={3}>
+            <Block>
+              <Image style={{ width: "100px" }}
+                src="logo.svg"
+              />
+            </Block>
+          </Columns.Column>
+          <Columns.Column >
+            <Heading>
+              Cool-load
+            </Heading>
+            <Heading subtitle>
+              Gerenciador de Containers
+            </Heading>
+          </Columns.Column>
+        </Columns>
+        <Columns>
+          <Columns.Column >
+            <Navbar style={{ borderRadius: "10px" }}>
+              <Navbar.Menu>
+                <Navbar.Container >
+                  <Navbar.Item hoverable arrowless>
+                    <Navbar.Link>Containers</Navbar.Link>
+                    <Navbar.Dropdown right>
+                      <Navbar.Item href="#">Lista</Navbar.Item>
+                      <Navbar.Divider />
+                      <Navbar.Item href="#">Novo</Navbar.Item>
+                      <Navbar.Item href="#">Editar</Navbar.Item>
+                      <Navbar.Item href="#">Remover</Navbar.Item>
+                    </Navbar.Dropdown>
+                  </Navbar.Item>
+                  <Navbar.Item hoverable arrowless>
+                    <Navbar.Link>Clientes</Navbar.Link>
+                    <Navbar.Dropdown right>
+                      <Navbar.Item href="#">Lista</Navbar.Item>
+                      <Navbar.Divider />
+                      <Navbar.Item href="#">Cadastrar</Navbar.Item>
+                      <Navbar.Item href="#">Editar</Navbar.Item>
+                      <Navbar.Item href="#">Remover</Navbar.Item>
+                    </Navbar.Dropdown>
+                  </Navbar.Item>
+                </Navbar.Container>
+                <Navbar.Container align="right">
+                  <Navbar.Item href="#">
+                    Relatórios
+                  </Navbar.Item>
+                </Navbar.Container>
+              </Navbar.Menu>
+            </Navbar>
+          </Columns.Column>
+        </Columns>
+      </Container>
+    </>
   );
 }
 
